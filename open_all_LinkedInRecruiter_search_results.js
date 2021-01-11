373c3e8002a919f7aa84d120ee70f23d6505fb30
Kevin Simonnet <k.simonnet@gmail.com>
2021-01-11 12:24:49 +1100

    Resolved the condition associated to checkIfViewed()

--- open_all_LinkedInRecruiter_search_results.js
+++ open_all_LinkedInRecruiter_search_results.js
@@ -8,23 +8,22 @@ function checkIfViewed(node_to_be_tested, filter_coll){
     var this_node = filter_coll.iterateNext();
     while (this_node) {
         if (node_to_be_tested.contains(this_node)) {
-            return true;
+            return false;
         }
         this_node = filter_coll.iterateNext();
     }
+    return true;
 };
 
-function storeUrls(){
+function getUrls(){
     var profile_container = document.getElementsByClassName("hp-core-temp profile-list ember-view")[0].getElementsByClassName("profile-list")[0].getElementsByTagName("li");
     var profile_urls_arr = [];
 
     for (let i = 0; i < profile_container.length; i++) {
-        var XPath = './/*[@type="eyeball-icon"]';   //with '.', the querying starts from the context node. '//' is the Recursive Descent Operator that matches elements anywhere in the document tree
+        var XPath = './/*[@type="eyeball-icon"]';   //with '.', the querying starts from the context node. '//' is the Recursive Descent Operator that matches elements anywhere in the document tree (https://developer.mozilla.org/en-US/docs/Web/API/Document/evaluate)
         var eyeball_coll = document.evaluate(XPath, document, null, XPathResult.ANY_TYPE, null); //XPathResult is a set of nodes
 
         if (checkIfViewed(profile_container[i], eyeball_coll)) {
-            //do nothing
-            } else {
             const profile_url = profile_container[i].getElementsByClassName("artdeco-entity-lockup__title")[0].getElementsByTagName("a")[0].href;
             profile_urls_arr.push(profile_url);
             }
@@ -33,8 +32,8 @@ function storeUrls(){
 }
 
 var urls_profiles_not_viewed = new Promise(function (resolve) {
-    resolve(storeUrls());
+    resolve(getUrls());
 });
 urls_profiles_not_viewed.then(function (urls_arr) {
-    urls_arr.forEach(elm => window.open(elm)) //console.log(elm)
+    urls_arr.forEach(elm => window.open(elm))
 });
